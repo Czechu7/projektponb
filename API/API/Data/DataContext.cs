e9f6023b3881098a3b92aa0e5c9594bd2a4f350e
Paweł <p.pawlik@itmcode.pl>
2024-11-24 17:28:06 +0100

    clean v2

--- API/API/Data/DataContext.cs
+++ API/API/Data/DataContext.cs
@@ -10,8 +10,6 @@ namespace API.Data
         }
 
         public DbSet<AppUser> Users { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<Cart> Carts { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
@@ -21,28 +19,7 @@ namespace API.Data
         .Property(u => u.Id)
         .ValueGeneratedOnAdd();
 
-            modelBuilder.Entity<Product>(entity =>
-            {
-                entity.HasKey(p => p.Id);
-                entity.Property(p => p.Name).IsRequired().HasMaxLength(100);
-                entity.Property(p => p.Price).IsRequired();
-                entity.Property(p => p.Quantity).IsRequired();
-            });
-
-            modelBuilder.Entity<Cart>(entity =>
-            {
-                entity.HasKey(c => c.Id);
-
-                entity.HasOne(c => c.Product)
-                      .WithMany()
-                      .HasForeignKey(c => c.ProductId)
-                      .OnDelete(DeleteBehavior.Cascade);
-
-                entity.HasOne(c => c.User)
-                      .WithMany()
-                      .HasForeignKey(c => c.UserId)
-                      .OnDelete(DeleteBehavior.Cascade);
-            });
+          
         }
     }
 }

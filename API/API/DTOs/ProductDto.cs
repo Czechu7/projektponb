f0cc6794e4abb39f8e07d2c5c7bc294372029805
Paweł <p.pawlik@itmcode.pl>
2024-11-24 17:06:13 +0100

    clean v1

--- API/API/DTOs/ProductDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.DTOs
-{
-    public class ProductDto
-    {
-        public string Name { get; set; }
-        public int Quantity { get; set; }
-        public int Price { get; set; }
-    }
-}

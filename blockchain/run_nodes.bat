f24c1cd35b16b0170e0c2bb3e90adf0d5e36c5af
Czechu7 <choocas0@gmail.com>
2025-01-09 18:44:04 +0100

    Update launch settings and expand node startup script for additional ports

--- blockchain/run_nodes.bat
+++ blockchain/run_nodes.bat
@@ -2,3 +2,6 @@
 start python run_app.py --port=5001
 start python run_app.py --port=5002
 start python run_app.py --port=5003
+start python run_app.py --port=5004
+start python run_app.py --port=5005
+start python run_app.py --port=5006

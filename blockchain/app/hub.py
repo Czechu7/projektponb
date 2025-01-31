8d39dedf531d43880ee8e1b06edef27d58ff50f6
kkolcz <k.kolcz@outlook.com>
2025-01-31 02:54:52 +0100

    naprawione

--- blockchain/app/hub.py
+++ blockchain/app/hub.py
@@ -4,6 +4,7 @@ from flask import Blueprint, jsonify, request
 import zlib
 import logging
 from  .models.blockchain import Blockchain
+import time
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -45,7 +46,9 @@ class FileUploadHub:
                 response = requests.post(f'http://127.0.0.1:5001/transactions/new', json={'transaction': transaction})
                 if response.status_code == 201:
                     logger.info("Transaction added to blockchain.")
-                    self.blockchain.mine_pending_transactions()
+                    # time.sleep(1)
+                    response = requests.get(f'http://127.0.0.1:5001/mine')
+                    # self.blockchain.mine_pending_transactions()
                 else:
                     logger.warning("Transaction was not added to blockchain.")
             else:
